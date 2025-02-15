cf7850f7715372351ac22cab6c575e9d8b2a1277
Pedro Nícolas Gomes de Souza <53797066+pedronicolasg@users.noreply.github.com>
2025-02-15 20:44:00 -0300

    Conn update

--- methods/conn.php
+++ methods/conn.php
@@ -1,17 +1,34 @@
 <?php
-
 $config = require 'config.php';
 
-$host = $config['db']['host'];
-$dbname = $config['db']['dbname'];
-$username = $config['db']['username'];
-$password = $config['db']['password'];
+if (!isset($config['db'])) {
+  die("Erro: Configurações do banco de dados não encontradas.");
+}
 
-$connString = "mysql:host=$host;port=11574;dbname=$dbname;sslmode=require";
+$dbConfig = $config['db'];
+
+$host = $dbConfig['host'];
+$dbname = $dbConfig['dbname'];
+$username = $dbConfig['username'];
+$password = $dbConfig['password'];
+$port = $dbConfig['port'];
+$sslrootcert = $dbConfig['sslrootcert'];
+
+if (!file_exists($sslrootcert)) {
+  die("Erro: Certificado SSL não encontrado em $sslrootcert.");
+}
+
+$dsn = "mysql:host=$host;port=$port;dbname=$dbname";
+
+$options = [
+  PDO::MYSQL_ATTR_SSL_CA => $sslrootcert,
+  PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
+];
 
 try {
-  $conn = new PDO($connString, $username, $password);
-
-} catch (Exception $e) {
-  echo "Error: " . $e->getMessage();
+  $conn = new PDO($dsn, $username, $password, $options);
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Habilita exceções para erros
+} catch (PDOException $e) {
+  die("Erro de conexão: " . $e->getMessage());
 }
+return $conn;
